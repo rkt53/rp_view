c1921756126488c1d61fa7aeb7a32e028beeda5e
rkt53 <rtabtiang@gmail.com>
2026-02-01 10:29:39 -0500

    added weather and alternating screen

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
 # https://learn.adafruit.com/making-a-pyportal-user-interface-displayio/the-full-code
 # SPDX-FileCopyrightText: 2020 Richard Albritton for Adafruit Industries
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 import time
@@ -8,7 +9,6 @@ import microcontroller
 import displayio
 import busio
 import gc
-# import neopixel
 
 import adafruit_adt7410
 import adafruit_touchscreen
@@ -28,19 +28,13 @@ from adafruit_esp32spi import adafruit_esp32spi
 from adafruit_esp32spi.adafruit_esp32spi_wifimanager import WiFiManager
 import neopixel
 from digitalio import DigitalInOut
+
 # Get wifi details and more from a settings.toml file
 # tokens used by this Demo: CIRCUITPY_WIFI_SSID, CIRCUITPY_WIFI_PASSWORD
-
-
 # If you are using a board with pre-defined ESP32 Pins:
 esp32_cs = DigitalInOut(board.ESP_CS)
 esp32_ready = DigitalInOut(board.ESP_BUSY)
 esp32_reset = DigitalInOut(board.ESP_RESET)
-
-# If you have an externally connected ESP32:
-# esp32_cs = DigitalInOut(board.D9)
-# esp32_ready = DigitalInOut(board.D10)
-# esp32_reset = DigitalInOut(board.D5)
 
 # Secondary (SCK1) SPI used to connect to WiFi board on Arduino Nano Connect RP2040
 if "SCK1" in dir(board):
@@ -49,20 +43,8 @@ else:
     spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 
-"""Use below for Most Boards"""
-status_pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
-"""Uncomment below for ItsyBitsy M4"""
-# status_pixel = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
-"""Uncomment below for an externally defined RGB LED (including Arduino Nano Connect)"""
-# import adafruit_rgbled
-# from adafruit_esp32spi import PWMOut
-# RED_LED = PWMOut.PWMOut(esp, 26)
-# GREEN_LED = PWMOut.PWMOut(esp, 27)
-# BLUE_LED = PWMOut.PWMOut(esp, 25)
-# status_pixel = adafruit_rgbled.RGBLED(RED_LED, BLUE_LED, GREEN_LED)
-
 # ------------- WifI Connection ------------- #
-
+status_pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
 ssid = getenv("CIRCUITPY_WIFI_SSID")
 password = getenv("CIRCUITPY_WIFI_PASSWORD")
 
@@ -92,8 +74,6 @@ icon_name = "Ruby"
 button_mode = 1
 switch_state = 0
 
-# For add ons
-TESTING = False
 TEXT_OUTPUT_MODE = False  # Set to True for console text output instead of display
 TIME_API = "http://worldtimeapi.org/api/ip"
 RP_URL = "https://api.radioparadise.com/api/nowplaying_list_v2022?chan=0&source=The%20Main%20Mix&player_id=&sync_id=chan_0&type=channel&mode=wip-channel&list_num=4"
@@ -134,13 +114,10 @@ def set_image(group, filename):
     print("Set image to ", filename)
     if group:
         group.pop()
-
     if not filename:
         return  # we're done, no icon desired
-
     image = displayio.OnDiskBitmap(filename)
     image_sprite = displayio.TileGrid(image, pixel_shader=image.pixel_shader)
-
     group.append(image_sprite)
 
 
@@ -183,8 +160,21 @@ def get_json(json_url: str, error_msg: str):
     """
     gc.collect()
     # print("Using Testing path")
-    response = wifi.get(json_url)
-    return response.json()
+    attempts = 0
+    while attempts < 5:
+        try:
+            response = wifi.get(json_url)
+            return response.json()
+        except (TimeoutError,
+                RuntimeError,
+                adafruit_requests.OutOfRetries) as e:
+            if attempts == 4:
+                import supervisor
+                supervisor.reload()
+            print(f"attempt {attempts} for {error_msg}: {e}")
+            attempts += 1
+            time.sleep(2)
+
 
 def set_time():
     """ Routine to set the time
@@ -221,8 +211,53 @@ def get_music(response_type: str = 'str') -> str:
             return item
     return "Error"
 
+def get_fahrenheit(celsius: float) -> float:
+    """ convert temperature to fahrenheit"""
+    return (celsius * 9/5) + 32
+
+def get_weather() -> str:
+    """ retrieve the weather"""
+    response = get_json(WEATHER, "get weather")
+    if response:
+        try:
+            temp = response['properties']['temperature']['value']
+            temp_float = float(temp)
+            temp_far = get_fahrenheit(temp_float)
+            result = f"{temp_far:.1f} F {temp} C"
+            return result
+        except (KeyError, TypeError) as e:
+            print(f"value error {e}")
+    return "failed to get weather"
+
 
 last_time = 0
+interval_info = [0, time.time(), [10, 30]]
+
+def interval_sequence() -> tuple[bool, int]:
+    """ advance frame by set number of seconds
+        return the next interval
+        
+        interval_info[0] is the current interval
+        interval_info[1] is the last time
+        :rtype: tuple[bool, int]
+    """
+    
+    def next_interval(current_interval, last_interval):
+        if current_interval == last_interval-1:
+            return 0
+        return current_interval + 1
+    
+    global interval_info
+    interval_list = interval_info[2]
+    delta = time.time() - interval_info[1]
+    if delta > interval_list[interval_info[0]]:
+        interval_info[0] = next_interval(interval_info[0], len(interval_list))
+        interval_info[1] = time.time()
+        return True, interval_info[0]
+    return False, interval_info[0]
+        
+    # current interval is interval
+
 
 def interval_elapsed(interval: int = 30):
     """ check if interval elapsed """
@@ -288,9 +323,9 @@ font_large.load_glyphs(b"abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123
 font_mid = bitmap_font.load_font("/fonts/luBS19.bdf")
 font_mid.load_glyphs(b"abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890- ()")
 # Text Label Objects
-time_data = Label(font, text="Time Data", color=0xE39300)
+time_data = Label(font_mid, text="Time Data", color=0xE39300)
 time_data.x = TABS_X + 2
-time_data.y = TABS_Y
+time_data.y = TABS_Y + 10
 view1.append(time_data)
 
 music_data = Label(font_mid, text="Music Data", color=0xFFFFFF)
@@ -304,7 +339,7 @@ music_rating = Label(font_large, text="0", color=0xFFFFFF,
     padding_bottom=8,
     padding_left=8,)
 music_rating.anchor_point = (1.0, 1.0)
-music_rating.anchored_position = (SCREEN_WIDTH - 40, SCREEN_HEIGHT - 40)
+music_rating.anchored_position = (SCREEN_WIDTH - 20, SCREEN_HEIGHT - 20)
 view2.append(music_rating)
 
 sensors_label = Label(font, text="Data View", color=0x03AD31)
@@ -421,12 +456,12 @@ layerVisibility("hide", splash, view2)
 layerVisibility("hide", splash, view3)
 
 # Update out Labels with display text.
-text_box(
-    time_data,
-    TABS_Y + 20,
-    "Time {}.".format(get_time()),
-    30,
-)
+# text_box(
+#     time_data,
+#     TABS_Y + 5,
+#     get_time(),
+#     30,
+# )
 
 # text_box(feed2_label, TABS_Y, "Tap on the Icon button to meet a new friend.", 18)
 # text_box(
@@ -436,12 +471,10 @@ text_box(
 #     28,
 # )
 
-# ------------- Initialization ------------- #
-if not TEXT_OUTPUT_MODE:
-    board.DISPLAY.root_group = splash
-last_time = time.time()
-set_time()
 
+def update_weather_panel():
+    weather_text = get_weather()
+    time_data.text = get_time() + "\n" + weather_text
 
 def update_rating(rating):
     """ update the rating """
@@ -469,7 +502,7 @@ def update_rating(rating):
     music_rating.text = str(rating)
 
 
-def update_music():
+def update_music() -> None:
     music_info = get_music("json")
 
     if TEXT_OUTPUT_MODE:
@@ -501,6 +534,13 @@ def update_music():
         else:
             music_data.text = "Loading error"
 
+# ------------- Initialization ------------- #
+if not TEXT_OUTPUT_MODE:
+    board.DISPLAY.root_group = splash
+last_time = time.time()
+set_time()
+update_weather_panel()
+
 # ------------- Code Loop ------------- #
 while True:
     touch = ts.touch_point
@@ -510,14 +550,22 @@ while True:
         touch, light, get_Temperature(adt)
         )
 
-    time_data.text = "Time {}".format(get_time())
+    #time_data.text = "Time {}".format(get_time())
 
+    interval_advance, next_panel = interval_sequence()
+    if interval_advance:
+        if next_panel == 0:
+            update_weather_panel()
+            switch_view(1)
+        elif next_panel == 1:
+            update_music()
+            switch_view(2)
 
     # Only update music data at specified interval (default 30 seconds)
-    if interval_elapsed(30):
-        update_music()
-        if view_live != 2 and not TEXT_OUTPUT_MODE:
-            switch_view(2)
+    # if interval_elapsed(30):
+    #     update_music()
+    #     if view_live != 2 and not TEXT_OUTPUT_MODE:
+    #         switch_view(2)
         # else:
         #     switch_view(1)
 
